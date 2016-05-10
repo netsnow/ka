a398a07abbd0de402626800d243bc9632bc31013
baixue <bai.xue@trans-cosmos.com.cn>
2016-05-10 20:00:05 +0800

    no message

--- resources/views/default/admin/index.blade.php
+++ resources/views/default/admin/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>房间</title>
+		<title>卡机</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta http-equiv="X-UA-Compatible"  content="IE=edge" />
 		<meta name="description" content="" />
