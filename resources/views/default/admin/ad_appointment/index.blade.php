676382ec8178898c64035f1808afbbcbe2c748ef
baixue <bai.xue@trans-cosmos.com.cn>
2016-05-16 20:25:54 +0800

    socket.io导入

--- resources/views/default/admin/ad_appointment/index.blade.php
+++ resources/views/default/admin/ad_appointment/index.blade.php
@@ -9,8 +9,10 @@
 	<!-- <meta http-equiv="refresh" content="15;url='/admin/ad_appointment/appoint'"> -->
 	<meta name="renderer" content="webkit">
 	<meta name="viewport" content="width=device-width, initial-scale=1,user-scalable=no" />
+
 	{!! style('/assets/admin/css/slider.css') !!}
 	{!! script('/assets/admin/js/index/jquery.js') !!}
+	{!! script('/assets/admin/js/socket.io/socket.io.js') !!}
 </head>
 <body style="background-color:#d7d7d7;margin:0">
 	<div id="wowslider-container1">
@@ -26,5 +28,34 @@
 
 	{!! script('/assets/admin/js/index/wowslider.js') !!}
 	{!! script('/assets/admin/js/index/script.js') !!}
+	{!! script('/assets/admin/js/layer/layer.js') !!}
+
+<script>
+$(document).ready(function () {
+    // 连接服务端
+    var socket = io('http://0.0.0.0:2120');
+    // 连接后登录
+    socket.on('connect', function(){
+    	socket.emit('login', 1);
+    });
+    // 后端推送来消息时
+    socket.on('new_msg', function(msg){
+         //$('#content').html('收到消息：'+msg);
+         //$('.notification.sticky').notify();
+         //location.href = '/'+msg;
+				 //alert("Hello World!");
+         layer.open({
+           type: 1,
+           title: 'hello',
+           content: '<img src="/1.jpg" />',
+           time: '5000'
+          });
+    });
+    // 后端推送来在线数据时
+    socket.on('update_online_count', function(online_stat){
+        $('#online_box').html(online_stat);
+    });
+});
+</script>
 </body>
 </html>
