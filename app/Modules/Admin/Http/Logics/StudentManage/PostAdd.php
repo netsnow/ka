3b6b968de691011a7a1519f495c84f191db068ae
baixue <bai.xue@trans-cosmos.com.cn>
2016-05-18 10:35:49 +0800

    语音合成ok

--- app/Modules/Admin/Http/Logics/StudentManage/PostAdd.php
+++ app/Modules/Admin/Http/Logics/StudentManage/PostAdd.php
@@ -50,12 +50,27 @@ class PostAdd extends \BaseLogic
   			$img = '/data/uploads/' . $newName;
 
   		}
+       //百度授权认证
         $baidu_auth = curl_init();
         curl_setopt($baidu_auth, CURLOPT_URL, "https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=B9Lu0wxY0T0B3ixtEy3VQ7Q7&client_secret=588b6fa0100126ccf530aadfb1e07992");
         curl_setopt($baidu_auth, CURLOPT_RETURNTRANSFER, 1 );
         $output = curl_exec($baidu_auth);
         $token=json_decode($output)->access_token;
         curl_close($baidu_auth);
+
+        //百度语音合成
+        $text = Request::input('real_name')."你好";
+        $baidu_yuyin = curl_init();
+        curl_setopt($baidu_yuyin, CURLOPT_URL, "http://tsn.baidu.com/text2audio?tex=".$text."&lan=zh&cuid=1111&ctp=1&tok=".$token);
+        curl_setopt($baidu_yuyin, CURLOPT_RETURNTRANSFER, 1 );
+        $output = curl_exec($baidu_yuyin);
+        curl_close($baidu_yuyin);
+        $targetDir = public_path() . '/data/audio/';
+        $filename = time() . '_' . rand( 1 , 1000000 ) . ".mp3";
+        $myaudiofile = fopen($targetDir.$filename, "w") or die("Unable to open file!");
+        fwrite($myaudiofile, $output);
+        fclose($myaudiofile);
+
 
         $newStudent = new Student;
         $newStudent->phone = Request::input('phone');
@@ -65,7 +80,7 @@ class PostAdd extends \BaseLogic
         $newStudent->card_num = Request::input('cardnum');
         $newStudent->store_name = Request::input('store_name');
         $newStudent->company_name = Request::input('company');
-        $newStudent->audio = $token;
+        $newStudent->audio = '/data/audio/'.$filename;
         if(Request::hasFile('img')){
   				$newStudent->img  = $img;
   			}
