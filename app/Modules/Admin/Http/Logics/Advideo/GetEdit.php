6a1f090904cf24b9e118a73b7d51073a9d1f353b
baixue <zhouqiufan@zhouqiufandeMacBook-Air.local>
2016-12-08 11:52:11 +0800

    walkman shell修正

---
+++ app/Modules/Admin/Http/Logics/Advideo/GetEdit.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Modules\Admin\Http\Logics\Advideo;
+
+use App\Eloquents\Ad;
+use Exception;
+use Request;
+
+class GetEdit extends \BaseLogic
+{
+    protected function execute()
+    {
+        
+    }
+
+
+}
--- message/start.sh
+++ message/start.sh
@@ -1,1 +1,2 @@
-/Applications/XAMPP/bin/php /Applications/XAMPP/xamppfiles/htdocs/message/start.php start -d
+/Applications/XAMPP/bin/php /Applications/XAMPP/xamppfiles/htdocs/ka/message/start.php start -d
+/opt/lampp/bin/php /opt/lampp/htdocs/ka/message/start.php start -d
