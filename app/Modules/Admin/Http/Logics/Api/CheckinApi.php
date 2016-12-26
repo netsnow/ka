c3b01054d4dcbe2e90f24423e52611a68374cfbe
baixue <zhouqiufan@zhouqiufandeMacBook-Air.local>
2016-12-26 15:41:33 +0800

    打卡只发一次

--- app/Modules/Admin/Http/Logics/Api/CheckinApi.php
+++ app/Modules/Admin/Http/Logics/Api/CheckinApi.php
@@ -28,7 +28,7 @@ class CheckinApi extends \BaseLogic
       $name = $student->real_name;
       $classname = $student->company_name;
       $studentid = $student->student_id;
-          //echo $img."|".$audio."|".$machineid;
+       //echo $img."|".$audio."|".$machineid;
 
       $today =date('Ymd',time());
       $today_time =date('YmdHi',time());
@@ -48,7 +48,6 @@ class CheckinApi extends \BaseLogic
 
       $checkindata = CheckinData::whereRaw('user_id = '.$studentid.' and checkin_date = '.$today)->first();
       //16点前正常刷卡,及16点后正常刷卡
-
       if($today_hour < 15 || $today_hour >= 15 && $checkindata){
 
           //调用websocket，把用户信息传给打卡终端广告机
@@ -56,32 +55,32 @@ class CheckinApi extends \BaseLogic
          //echo "|roomid=".$room->room_id;
        $uriname = urlencode($name);
        $uriclassname = urlencode($classname);
-         //echo $name;
        $ch = curl_init();
        curl_setopt($ch, CURLOPT_URL, "http://0.0.0.0:2121?type=publish&to=".$room->room_id."&content=".$img."|".$audio."|".$uriname."|".$uriclassname."|".$this->cardno."|".$today_day."|".$today_hour."|".$today_min);
        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
        $output = curl_exec($ch);
        curl_close($ch);
-
-       try
-       {
-         $newCheckinData->save();
-         echo "ok";
-       }
-       catch (Exception $e)
-       {
-         echo $e->getMessage();
-       }
+       if(!$checkindata){
+         try
+         {
+           $newCheckinData->save();
+           echo "ok";
+         }
+         catch (Exception $e)
+         {
+           echo $e->getMessage();
+         }
           //调用短信接口，发送短信
-       $phone = $student->store_name;
-       $content = "家长你好，".$name."已刷卡入园。";
-       $url = "http://115.28.112.245:8082/SendMT/SendMessage?UserName=hedongyiyou&UserPass=123456&Mobile=".$phone."&Content=".$content;
+         $phone = $student->store_name;
+         $content = "家长你好，".$name."已刷卡入园。";
+         $url = "http://115.28.112.245:8082/SendMT/SendMessage?UserName=hedongyiyou&UserPass=123456&Mobile=".$phone."&Content=".$content;
           //echo $url;
-       $message = curl_init();
-       curl_setopt($message, CURLOPT_URL, $url);
-       curl_setopt($message, CURLOPT_RETURNTRANSFER, 1 );
-       $output2 = curl_exec($message);
-       curl_close($message);
+         $message = curl_init();
+         curl_setopt($message, CURLOPT_URL, $url);
+         curl_setopt($message, CURLOPT_RETURNTRANSFER, 1 );
+         $output2 = curl_exec($message);
+         curl_close($message);
+       }
      }else{
 
           //调用websocket，把用户信息传给打卡终端广告机
